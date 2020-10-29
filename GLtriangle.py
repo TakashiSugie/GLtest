2e1b6f8f6cb5476168664aef8ed526df24a207c2
TakashiSugie <zako0808@gmail.com>
2020-10-29 15:00:52 +0900

    point cloud comp

--- GLtriangle.py
+++ GLtriangle.py
@@ -1,27 +1,73 @@
 #!/usr/bin/python
 
+import cv2
 from OpenGL.GL import *
 from OpenGL.GLUT import *
+import numpy as np
 
 
-def draw():
-    glClearColor(0.0, 0.0, 1.0, 0.0)
+img = cv2.imread("data/04_04.png")
+depthImg = cv2.imread("data/04_04_depth.png", 0)
+width = img.shape[1]
+height = img.shape[0]
+maxD = np.max(depthImg)
+minD = np.min(depthImg)
+ratio = 0.00001
+verts = []
+
+
+def setVerts(img, depthImg):
+    global verts
+    for x in range(img.shape[1]):
+        for y in range(img.shape[0]):
+            colors = [
+                float(img[y][x][0] / 255.0),
+                float(img[y][x][1] / 255.0),
+                float(img[y][x][2] / 255.0),
+            ]
+            # vert = np.array([x, y, depthImg[x][y], colors[0], colors[1], colors[2]])
+            X, Y, Z = calcVert(x, y)
+            vert = np.array([X, Y, Z, colors[0], colors[1], colors[2]])
+            verts.append(vert)
+    return verts
+
+
+def calcVert(x, y):
+    X = (2.0 * float(x) / float(width)) - 1
+    Y = 1 - (2.0 * float(y) / float(height))
+    Z = ratio * float(maxD - minD) * float(depthImg[y][x])
+    return X, Y, Z
+
+
+def myDraw():
+    global verts
+    glClearColor(0.0, 0.0, 0.0, 0.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    glBegin(GL_TRIANGLES)
-    glVertex(-1, -1)
-    glVertex(1, -1)
-    glVertex(0, 1)
+    glPointSize(1)
+    glBegin(GL_POINTS)
+    print(len(verts))
+    for vert in verts:
+        glColor3d(vert[3], vert[4], vert[5])
+        glVertex3f(vert[0], vert[1], vert[2])
     glEnd()
-
     glFlush()
     glutSwapBuffers()
 
 
-glutInit(sys.argv)
-glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
-glutInitWindowSize(320, 240)
-glutCreateWindow("PyOpenGL 2")
-glutDisplayFunc(draw)
+def setting():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+    glutInitWindowSize(320, 240)
+    glutCreateWindow("PyOpenGL 2")
 
-glutMainLoop()
+
+def rendering():
+    glutDisplayFunc(myDraw)
+    glutMainLoop()
+
+
+if __name__ == "__main__":
+    setting()
+    verts = setVerts(img, depthImg)
+    rendering()
