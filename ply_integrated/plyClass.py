4ab12164e2e4107d0e99a549c0064515da7424e8
TakashiSugie <zako0808@gmail.com>
2020-11-12 16:02:50 +0900

    class Ply improved

--- ply_integrated/plyClass.py
+++ ply_integrated/plyClass.py
@@ -1,35 +1,10 @@
+import numpy as np
+
+
 class Ply:
     def __init__(self, mesh_fi):
         self.plyName = mesh_fi
         self.ClassReadPly()
-        # self.save_fi = save_fi
-
-    #
-    def ClassWritePly(self, save_fi):
-        self.convertForWrite()
-        print("Writing mesh file %s ..." % save_fi)
-        with open(save_fi, "w") as ply_fi:
-            ply_fi.write("ply\n" + "format ascii 1.0\n")
-            ply_fi.write("comment H " + str(self.Height) + "\n")
-            ply_fi.write("comment W " + str(self.Width) + "\n")
-            ply_fi.write("comment hFov " + str(self.hFov) + "\n")
-            ply_fi.write("comment vFov " + str(self.vFov) + "\n")
-            ply_fi.write("element vertex " + str(self.num_vertex) + "\n")
-            ply_fi.write(
-                "property float x\n"
-                + "property float y\n"
-                + "property float z\n"
-                + "property uchar red\n"
-                + "property uchar green\n"
-                + "property uchar blue\n"
-                + "property uchar alpha\n"
-            )
-            ply_fi.write("element face " + str(self.num_face) + "\n")
-            ply_fi.write("property list uchar int vertex_index\n")
-            ply_fi.write("end_header\n")
-            ply_fi.writelines(self.v_line + "\n")
-            ply_fi.writelines(self.f_line)
-        ply_fi.close()
 
     def ClassReadPly(self):
         self.ply_fi = open(self.plyName, "r")
@@ -62,21 +37,61 @@ class Ply:
         for f_info in face_infos:
             self.f_infos.append(f_info)
 
-    def convertForWrite(self):
-        v_list = []
+    def ClassWritePly(self, save_fi, npyPath=None):
+        if npyPath == None:
+            self.convertForWrite()
+        else:
+            self.dotsM(npyPath)
+
+        print("Writing mesh file %s ..." % save_fi)
+        with open(save_fi, "w") as ply_fi:
+            ply_fi.write("ply\n" + "format ascii 1.0\n")
+            ply_fi.write("comment H " + str(self.Height) + "\n")
+            ply_fi.write("comment W " + str(self.Width) + "\n")
+            ply_fi.write("comment hFov " + str(self.hFov) + "\n")
+            ply_fi.write("comment vFov " + str(self.vFov) + "\n")
+            ply_fi.write("element vertex " + str(self.num_vertex) + "\n")
+            ply_fi.write(
+                "property float x\n"
+                + "property float y\n"
+                + "property float z\n"
+                + "property uchar red\n"
+                + "property uchar green\n"
+                + "property uchar blue\n"
+                + "property uchar alpha\n"
+            )
+            ply_fi.write("element face " + str(self.num_face) + "\n")
+            ply_fi.write("property list uchar int vertex_index\n")
+            ply_fi.write("end_header\n")
+            ply_fi.writelines(self.v_line + "\n")
+            ply_fi.writelines(self.f_line)
+        ply_fi.close()
+
+    def convertForWrite(self):  # listで来たv_infosの各要素を取り出して、変換を行わずにstr型に変換する
+        self.v_line = "".join(self.v_infos)
+        self.f_line = "".join(self.f_infos)
+
+    def dotsM(self, npyPath):  # listで来たv_infosの各要素を取り出して、変換Mを行う、その後、str型に変換する
+        self.v_list = []
+        M = np.load(npyPath)
         for v_info in self.v_infos:
             str_info = [float(v) for v in v_info.split("\n")[0].split(" ")]
             if len(str_info) == 6:
                 vx, vy, vz, r, g, b = str_info
             else:
                 vx, vy, vz, r, g, b, hi = str_info
-            v_list.append(
-                " ".join(list(map(str, [vx, vy, vz, int(r), int(g), int(b), int(hi)])))
+            oldV = np.array((vx, vy, vz, 1))
+            NewV = np.dot(M, oldV)
+            NewVx, NewVy, NewVz = NewV
+            self.v_list.append(
+                " ".join(
+                    list(
+                        map(str, [NewVx, NewVy, NewVz, int(r), int(g), int(b), int(hi)])
+                    )
+                )
             )
-        self.v_line = "\n".join(v_list)
+        self.v_line = "\n".join(self.v_list)
         self.f_line = "".join(self.f_infos)
-    def dotsM(self, M):
-
 
 
 if __name__ == "__main__":
